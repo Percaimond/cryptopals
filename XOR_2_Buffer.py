057e6f6d7afe4280b24ab35a27f9580b08fb6281
danielweissenborn@outlook.de <Fut32V?$rfin>
2021-01-04 18:48:46 +0100

    Working connection to git-repository

--- XOR_2_Buffer.py
+++ XOR_2_Buffer.py
@@ -3,7 +3,7 @@ import binascii
 string1 = "1c0111001f010100061a024b53535009181c"
 string2 = "686974207468652062756c6c277320657965"
 
-raw = binascii.unhexlify(string1)
+raw = binascii.unhexlify(string1)#ok
 raw2 = binascii.unhexlify(string2)
 a_list = bytes([_a ^ _b for _a, _b in zip(raw,raw2)])
 b_list = binascii.hexlify(a_list)
